06f824d83a542ff4c423e054858f7aa624626cc8
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 16:25:29 -0400

    server functional

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use.express(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
-// app.use(routes)
+app.use(routes);
 
 db.once('open', () =>{
   app.listen(PORT, () => console.log(`API server running on port ${PORT}!`))
