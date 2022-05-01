6b9b28296c5050cdd07aa491317e028bc5b03c84
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 17:14:04 -0400

    createUser, getUser functional

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,20 @@
 const { ObjectId } = require('mongoose').Types;
 const { User } = require('../models');
 
-module.exports = {
-  async getUsers (req, res) {
+module.exports = { // CRUD Operations:
+  async createUser (req, res) { // create single User
+    // Expect -> req.body: {'username': <string>, 'email': <string>}
+    try {
+      const user = await User.create(req.body);
+
+      return res.json(user)
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  async getUsers (req, res) { // get all users
     try {
       const users = await User.find()
         .populate('friends', '_id username email');
@@ -10,13 +22,38 @@ module.exports = {
       return res.json(users);
     } catch (err) {
       console.log(err);
-
       return res.status(500).json(err);
     }
   },
 
-  async addFriend (req, res) {
-    console.log(req.params);
+  async getUser (req, res) { // get single user by _id
+    try {
+      const user = await User.findById(req.params.userId)
+        .populate('friends', 'username');
+
+      return res.json(user);
+    } catch(err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  
+  async updateUser (req, res) { // add user to user.friends by _id
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: ObjectId(req.params.userId)},
+        { $addToSet: { friends: ObjectId(req.body)}},
+        { new: true }
+      ).populate('friends', 'username');
+
+      return res.json(updatedUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  async addFriend (req, res) { // add user to user.friends by _id
     try {
       const updatedUser = await User.findOneAndUpdate(
         { _id: ObjectId(req.params.userId)},
@@ -27,7 +64,6 @@ module.exports = {
       return res.json(updatedUser);
     } catch (err) {
       console.log(err);
-
       return res.status(500).json(err);
     }
   }
