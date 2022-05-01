8e8241654463fa9ac4a13ac9d947415a350a2b4b
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 17:40:14 -0400

    delete route connected

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 
 const {
-  createUser, getUsers, getUser, updateUser, addFriend
+  createUser, getUsers, getUser, updateUser, addFriend, deleteUser
 } = require('../../controllers/userController'); 
 
 router.route('/') // CREATE single User, READ all Users
   .post(createUser).get(getUsers);
 
-router.route('/:userId') // READ single User by _id
-  .get(getUser).put(updateUser);
+router.route('/:userId') // READ, UPDATE, & DELETE single User by _id
+  .get(getUser).put(updateUser).delete(deleteUser);
 
 
 router.route('/:userId/friends/:friendId') // UPDATE User.friends by _id
