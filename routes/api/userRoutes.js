38b4305cbfa7720b014d9cfaaa61209513ba6773
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 17:14:47 -0400

    createUser, getUser, updateUser connected

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
 const {
-  getUsers, addFriend
-} = require('../../controllers/userController');
+  createUser, getUsers, getUser, updateUser, addFriend
+} = require('../../controllers/userController'); 
 
-router.route('/').get(getUsers);
+router.route('/') // CREATE single User, READ all Users
+  .post(createUser).get(getUsers);
 
-router.route('/:userId/friends/:friendId').put(addFriend);
+router.route('/:userId') // READ single User by _id
+  .get(getUser).put(updateUser);
+
+
+router.route('/:userId/friends/:friendId') // UPDATE User.friends by _id
+  .put(addFriend);
 
 module.exports = router;
