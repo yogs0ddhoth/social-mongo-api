8ea4b7413b1d6a97c4fc7403a357f830d8c8a676
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 16:25:12 -0400

    userRoutes connected

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+
 
 // Routes:
 // /users
 // /users/:userId/friends/:friendId
-
+router.use('/users', userRoutes);
 // /thoughts
 // /thoughts/:thoughtId/reactions
+module.exports = router;
