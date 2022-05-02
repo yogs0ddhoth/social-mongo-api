4cd2a021d8eaf4690b75f2c68d37e1c5be4f01b5
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 21:10:36 -0400

    model created

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // TODO: make getter
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
