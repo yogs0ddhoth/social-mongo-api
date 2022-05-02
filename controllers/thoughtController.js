23614ef328c36081ad42872eff4cd82d11672cbc
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 21:11:16 -0400

    createThought functional

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,2 +1,25 @@
 const { ObjectId } = require('mongoose').Types;
-const { Thought, Reaction } = require('../models');
+const { User, Thought } = require('../models');
+
+module.exports = {
+  async createThought (req, res) {
+    try {
+      const user = await Thought.create(req.body);
+      // console.log(user);
+
+      const updatedUser = await User.findOneAndUpdate(
+        { username: user.username},
+        { $addToSet: { thoughts: user._id}},
+        { new: true }
+      )
+      //  .populate('friends', 'username')
+      //  .populate('thoughts')
+      ;
+      console.log(updatedUser)
+      return res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
