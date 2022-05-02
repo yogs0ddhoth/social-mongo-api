210230221eda039977d7982c1f1376825962280b
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 21:12:31 -0400

    controls connected, createThought active

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
 const {
+  createThought
+} = require('../../controllers/thoughtController');
 
-} = require('../../controllers/thoughtController')
+// /thoughts
+router.route('/')
+  .post(createThought);
+
+// /thoughts/:thoughtId/reactions
+router.route('/:thoughtId')
+
+module.exports = router;
