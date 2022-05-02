3843f975bffd527eb359874d125d3a58430bdc7a
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-01 20:07:25 -0400

    unused code deleted

--- models/User.js
+++ models/User.js
@@ -1,5 +1,3 @@
-// // connection for testing model
-// const connection = require('../config/connection');
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
@@ -28,41 +26,6 @@ const userSchema = new Schema(
   }
 );
 
-// // Model Test:
-// const testUsers = [
-//   {username: 'A1', email: 'A2',},
-//   {username: 'B1', email: 'B2',},
-//   {username: 'C1', email: 'C2',},
-//   {username: 'D1', email: 'D2',},
-//   {username: 'E1', email: 'E2',}
-// ]
-
-// connection.on('error', (err) => err);
-
-// connection.once('open', async () => {
-//   try {
-//     // // Drop existing collection
-//     // await User.deleteMany({});
-//     // console.log('Users dropped');
-
-//     // // Seed collection
-//     // await User.collection.insertMany(testUsers);
-//     // console.log('Users seeded');
-
-//     // // populate single collection
-//     // await User.findOneAndUpdate(
-//     //   { _id: '626ed479018a61ee21a27e06'},
-//     //   { $addToSet: { friends: '626ed479018a61ee21a27e07'}},
-//     //   { new: true }
-//     // );
-//     // console.log('friends updated');
-//   } catch (err) {
-//     console.log(err);
-//   };
-//   process.exit(0);
-// });
-
-// 
 userSchema
   .virtual('friendCount')
   .get(function () {
